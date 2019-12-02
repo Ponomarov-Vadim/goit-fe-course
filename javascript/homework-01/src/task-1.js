029e9609e91ed662c539713f035ac321f9304ffb
Vadim Ponomarov <remary300@gmail.com>
2019-12-02 21:23:04 +0200

    update homework 1 task-1

--- javascript/homework-01/src/task-1.js
+++ javascript/homework-01/src/task-1.js
@@ -5,6 +5,3 @@ let price = 1000;
 console.log(`Выбран ${name} , цена за штуку ${price} кредитов`);
 price = 2000;
 console.log(`Выбран ${name} , цена за штуку ${price} кредитов`);
-
-let res = true && 5 > 3 && "javascript";
-console.log(res);
