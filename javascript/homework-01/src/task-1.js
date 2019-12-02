8acf942c418a723b8da5bed760f8b8444bcd7406
Vadim Ponomarov <remary300@gmail.com>
2019-12-02 21:21:03 +0200

    update homework 1

---
+++ javascript/homework-01/src/task-1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let name = "Генератор защитного поля";
+let price = 1000;
+console.log(`Выбран ${name} , цена за штуку ${price} кредитов`);
+price = 2000;
+console.log(`Выбран ${name} , цена за штуку ${price} кредитов`);
+
+let res = true && 5 > 3 && "javascript";
+console.log(res);
